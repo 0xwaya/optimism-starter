43fdc951e2c65ac19a3b59c8353a178f7f14a35b
e.mercado <admin@wayalabs.com>
2023-03-21 16:34:36 -0400

    commit to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
-
 import { Attestooooooor } from "./components";
 
 export function App() {
@@ -13,29 +12,99 @@ export function App() {
   }, []);
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <h1 style={{ marginTop: 20 }}>GOT SPORK?</h1>
+    <main style={{
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      justifyContent: 'space-between',
+      alignItems: 'stretch',
+      background: 'linear-gradient(to bottom right, #333333, #555555)',
+      color: 'white',
+      padding: '1em',
+      flex: '1 1 auto',
+      maxWidth: '100%',
 
-      <div style={{ marginTop: 40 }}>
-        <ConnectButton />
+    }}>
+      {/* Title and connect button container */}
+      <div style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        background: 'linear-gradient(to bottom right, #333333, #555555)',
+        color: 'white',
+        padding: '1em',
+        flex: '1 1 auto',
+        maxWidth: '100%',
+      }}>
+        {/* Title */}
+        <h1 style={{
+          fontSize: '2em',
+          fontWeight: 'bold',
+          color: 'white',
+          textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+
+        }}>GOT SPORK?</h1>
+
+        {/* Connect wallet button */}
+        <div style={{ 
+          display: 'flex',
+          alignItems: 'center'
+        }}>
+          <ConnectButton style={{ margin: '20px 0', marginRight: '20px' , background: 'linear-gradient(to bottom right, #333333, #555555)', color: 'white', border: 'none', borderRadius: '10px', padding: '10px 20px', fontSize: '1em', fontWeight: 'bold', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }} />  
+        </div>
       </div>
 
-      {!isLoading && (
-        <>
-          <hr style={{ margin: '50px 0' }} />
-          {isConnected && (
-            <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+      {/* Content container */}
+      <div style={{
+        background: 'linear-gradient(to bottom right, #888888, #cccccc)',
+        color: 'white',
+        padding: '1em',
+        flex: '3 1 auto',
+        maxWidth: '100%',
+
+      }}>
+        {isLoading ? (
+          <div style={{
+            fontSize: '2em',
+            fontWeight: 'bold',
+            color: 'white',
+            textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+            textAlign: 'center',
+            marginTop: '20px',
+
+          }}>Loading...</div>
+        ) : (
+          <>
+            {isConnected ? (
               <Attestooooooor />
-            </div>
-          )}
-          {!isConnected && (
-            <div style={{ marginTop: 20, fontStyle: 'italic' }}>
-              Please connect your wallet to proceed
-            </div>
-          )}
-          <hr style={{ margin: '50px 0' }} />
-        </>
-      )}
-    </div>
+            ) : (
+              <div style={{ marginTop: 20, fontStyle: 'italic', color: 'white' }}>
+                Please connect your wallet to proceed
+              </div>
+            )}
+          </>
+        )}
+      </div>
+
+      {/* Footer */}
+      <div style={{
+        textAlign: 'center',
+        background: 'linear-gradient(to bottom right, #333333, #555555)',
+        color: 'white',
+        fontSize: '14px',
+        padding: '1em',
+        flex: '1 1 auto',
+        maxWidth: '100%',
+
+      }}>
+      <footer>
+        <div style={{ textAlign: 'center', color: 'white', fontSize: '14px', padding: '1em', flex: '1 1 auto', maxWidth: '100%' }}>
+          © 2023 GOT SPORK? built by 0xwaya.&nbsp;
+          <a href="https://github.com/0xwaya/opatts">opatts</a>.
+        </div>
+      </footer>
+  
+      </div>
+    </main>
   );
 }
